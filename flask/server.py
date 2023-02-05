63825e20849ce5587afe0df0505bafd9e9fbbb3d
Will Quackenbush <willyquack@github.com>
2023-02-05 13:24:54 -0600

    Removed depends from compose

--- flask/server.py
+++ flask/server.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask, render_template, request, json
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from database.database import Database
 
 database: Database
@@ -9,16 +9,25 @@ app = Flask(__name__,template_folder="../angular/dist/ymca-schedule")
 CORS(app)
 
 @app.route("/")
+@cross_origin()
 def hello():
     return render_template("index.html")
 
 @app.route("/test", methods=['GET'])
+@cross_origin()
 def test():
     return "it has been changed"
 
 @app.route("/database/users", methods=['GET'])
+@cross_origin()
 def get_users():
     return database.get_all_users()
+
+@app.route("/register", methods=['POST'])
+@cross_origin()
+def register():
+    data = request.get_json()
+    return database.register(data)
 
 if __name__ == "__main__":
     database = Database()
