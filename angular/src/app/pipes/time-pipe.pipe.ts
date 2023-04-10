c67a68029ace54daea5d22cbdadc993f49195c6f
Andrew Perrin <perrin2678@uwlax.edu>
2023-04-10 11:19:32 -0500

    added home page

--- angular/src/app/pipes/time-pipe.pipe.ts
+++ angular/src/app/pipes/time-pipe.pipe.ts
@@ -4,7 +4,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class Convert24To12Pipe implements PipeTransform {
   transform(date: string): string {
     var hours = ((Number(date.split(':')[0]) + 11) % 12 + 1);
-    console.log(hours)
     var res: string = hours + ':' + date.split(':')[1];
     return res + ((Number(date.split(':')[0]) > 11) ? ' PM' : ' AM');
   }
